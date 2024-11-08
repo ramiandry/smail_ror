3fe6af156f40ee3f6b45d1f07617d3e24b93cec7
ramiandry <nyavotraa@gmail.com>
2024-11-08 10:43:50 +0300

    layout and register

--- app/controllers/receptions_controller.rb
+++ app/controllers/receptions_controller.rb
@@ -1,40 +1,55 @@
 class ReceptionsController < ApplicationController
-  before_action :set_reception, only: [ :show, :destroy ]
+  # Affiche toutes les réceptions
   def index
     @receptions = Reception.all
   end
 
+  # Affiche une réception spécifique
   def show
+    @reception = Reception.find(params[:id])
   end
 
+  # Affiche le formulaire pour créer une nouvelle réception
+  def new
+    @reception = Reception.new
+  end
+
+  # Crée une nouvelle réception
   def create
     @reception = Reception.new(reception_params)
-
     if @reception.save
-      if params[:pieces_jointes]
-        params[:pieces_jointes].each do |file|
-          @reception.pieces_jointes.attach(file)
-        end
-      end
-
       redirect_to @reception, notice: "Réception créée avec succès."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  # Affiche le formulaire pour modifier une réception existante
+  def edit
+    @reception = Reception.find(params[:id])
+  end
+
+  # Met à jour une réception existante
+  def update
+    @reception = Reception.find(params[:id])
+    if @reception.update(reception_params)
+      redirect_to @reception, notice: "Réception mise à jour avec succès."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # Supprime une réception
   def destroy
+    @reception = Reception.find(params[:id])
     @reception.destroy
     redirect_to receptions_path, notice: "Réception supprimée avec succès."
   end
 
   private
 
-  def set_reception
-    @reception = Reception.find(params[:id])
-  end
-
+  # Paramètres sécurisés
   def reception_params
-    params.require(:reception).permit(:nom, :email, :date_reception, pieces_jointes: [])
+    params.require(:reception).permit(:utilisateur_id, :email_id)
   end
 end
