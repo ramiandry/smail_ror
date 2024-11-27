39595550bd0f679b2171ceba51ae5c1f0d88a151
ramiandry <nyavotraa@gmail.com>
2024-11-27 13:44:10 +0300

    bg

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,5 +3,10 @@ require "sidekiq-cron"
 schedule_file = "config/schedule.yml"
 
 if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  begin
+    schedule = YAML.load_file(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash(schedule || {})
+  rescue StandardError => e
+    Rails.logger.error "Erreur lors du chargement des tâches cron : #{e.message}"
+  end
 end
